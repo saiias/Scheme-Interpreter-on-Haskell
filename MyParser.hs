71fdfe4a77f8d93d2ab018138058ed86feccbe73
sai <sa.nevermind@gmail.com>
2012-05-10 19:10:10 +0900

    1章まで

--- MyParser.hs
+++ MyParser.hs
@@ -1,14 +1,71 @@
 import System.Environment
 import Text.ParserCombinators.Parsec hiding(spaces)
+import Control.Monad
     
+data LispVal = Atom String
+             |List [LispVal]
+             |DottedList[LispVal] LispVal
+             |Number Integer
+             |String String
+             |Bool Bool
+
 symbol::Parser Char
 symbol=oneOf"!#$%&|*+/:<=>?@^_~"
-
+        
+spaces::Parser()
+spaces = skipMany1 space
+        
 readExpr::String->String
-readExpr input = case parse symbol "lisp" input of
-                   Left err ->"No math" ++ show err
-                   Right val ->"Found value"
+readExpr input = case parse parseExpr  "lisp" input of
+                   Left err ->"No match: " ++ show err
+                   Right _ ->"Found value"
 
+parseString::Parser LispVal
+parseString = do
+  char '"'
+  x<-many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom::Parser LispVal
+parseAtom=do
+  first <-letter<|>symbol
+  rest <- many(letter<|>digit<|>symbol)
+  let atom = first:rest
+  return $ case atom of
+             "#t"->Bool True
+             "#f"->Bool False
+             _ ->Atom atom
+
+parseNumber::Parser LispVal
+parseNumber = liftM(Number .read) $ many1 digit
+
+parseExpr::Parser LispVal
+parseExpr=parseAtom
+           <|>parseString
+           <|>parseNumber
+           <|>parseQuoted
+           <|>do char '('
+                 x<-try parseList<|>parseDottedList
+                 char ')'
+                 return x
+             
+
+parseList:: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList:: Parser LispVal
+parseDottedList = do
+  head <- endBy parseExpr spaces
+  tall <- char '.' >> spaces >> parseExpr
+  return $ DottedList head tall
+                  
+parseQuoted:: Parser LispVal
+parseQuoted = do
+  char '\''
+  x<-parseExpr
+  return $ List[Atom "quote",x]
+         
 main::IO()
 main = do
   args<-getArgs
